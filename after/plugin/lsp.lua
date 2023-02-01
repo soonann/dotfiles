20fce3ddd96cb2afccb89e58a213f4667d8e7223
soonann <sudo@soonann.dev>
2023-02-01 17:11:59 +0800

    feat: added commented custom lsp after script

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,13 @@
+-- -- Learn the keybindings, see :help lsp-zero-keybindings
+-- -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
+-- local lsp = require('lsp-zero')
+-- lsp.preset('recommended')
+-- 
+-- -- When you don't have mason.nvim installed
+-- -- You'll need to list the servers installed in your system
+-- lsp.setup_servers({ 'tsserver', 'eslint' })
+-- 
+-- -- (Optional) Configure lua language server for neovim
+-- lsp.nvim_workspace()
+-- 
+-- lsp.setup()
