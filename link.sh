23e7ed340796d8f8da823d7d5d5f491d176209e0
soonann <sudo@soonann.dev>
2023-05-08 13:55:18 +0800

    fix: fixed recursive symlink command

--- link.sh
+++ link.sh
@@ -3,10 +3,11 @@ CURR=$(pwd)
 
 # -- create sym link for
 # nvim dotfiles
-ln -s $CURR/nvim ~/.config/nvim
+ln -s $CURR/nvim ~/.config/
 
 # alacritty dotfiles
-ln -s $CURR/alacritty ~/.config/alacritty
+ln -s $CURR/alacritty ~/.config/
 
 # tmux config dotfiles
-ln -s $CURR/tmux/.tmux.conf ~/.tmux.conf
+ln -s $CURR/tmux/.tmux.conf ~/
+ln -s $CURR/tmux/tmux-sessionizer ~/.local/bin/
