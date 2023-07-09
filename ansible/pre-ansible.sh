002eeaad37157348e4a9771afac042b56c79f7bd
soonann <sudo@soonann.dev>
2023-07-09 22:41:13 +0800

    feat: rancher, docker and kubectl install scripts

--- ansible/pre-ansible.sh
+++ ansible/pre-ansible.sh
@@ -2,6 +2,15 @@
 # you should have already made sure the following are working:
 # 1. sudo access - add your current user to the sudo/wheel group and ensure they're able to run privileged commands
 # 2. git over ssh - download a browser of your choice and add a new public key to your git account
-# 3. git clone project - clone the project to your home directory
-# 4. ansible installed - https://docs.ansible.com/ansible/latest/installation_guide/index.html
+# 3. cloned dotfiles with git - clone the project to your home directory
+# 4. rc.d - added the following script to your .bashrc or .zshrc
+#if [ -d ~/.rc.d ]; then
+# for rc in ~/.rc.d/*; do
+#	 if [ -f "$rc" ]; then
+#		 . "$rc"
+#	 fi
+# done
+#fi
+#unset rc
+
 sudo apt install ansible
