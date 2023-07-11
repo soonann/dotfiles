b066a20d0b0f3916a9fcb7f7b5d3c8e6c2196dae
sudo@soonann.dev <sudo@soonann.dev>
2023-07-11 23:38:46 +0800

    refactor: moved key repeat and interval to gsettings

--- wm/gnome/gsettings-bindings.sh
+++ wm/gnome/gsettings-bindings.sh
@@ -34,7 +34,8 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 "['<Alt><Shif
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Alt><Shift>0']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys search "['<Alt>D']"
 gsettings set org.gnome.shell.keybindings show-screenshot-ui "['<Alt><Shift>S']"
-
+gsettings set org.gnome.desktop.peripherals.keyboard delay 280
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 40
 
 # aesthetics
 gsettings set org.yorba.shotwell.preferences.ui use-dark-theme true
