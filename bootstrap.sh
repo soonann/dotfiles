af372f1f9feec3583c31204dcbf2eae9f6496bb3
soonann <mail@soonann.dev>
2024-12-19 13:13:30 +0800

    refactor: added initial playbooks and reorganised project strucure

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install git and ansible
+sudo dnf install -y git ansible stow
+
+# Clone dotfiles
+# git clone git@github.com:soonann/dotfiles.git
+
+# Install ansible community collections and roles
+ansible-galaxy install -r requirements.yml
+
+echo 'You can now apply the playbook with:'
+echo 'ansible-playbook -K -c local -i localhost, main.yml'
